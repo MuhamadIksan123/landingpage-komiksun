9b274b0cd94b6f2bb254563ec384868df9d6cb63
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-09 14:35:23 +0700

    update: loader komik

--- components/CardKomikNoTitle/index.js
+++ components/CardKomikNoTitle/index.js
@@ -6,6 +6,17 @@ import { formatDate } from '../../utils/formatDate';
 
 export default function CardEvent({ dataKomik }) {
   const [currentPage, setCurrentPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    // Clean up the timeout on component unmount
+    return () => clearTimeout(timeout);
+  }, []);
 
   const handlePageClick = (data) => {
     setCurrentPage(data.selected);
@@ -19,31 +30,35 @@ export default function CardEvent({ dataKomik }) {
   return (
     <section className="grow-komik">
       <div className="container">
-        <div className="mt-5 row gap">
-          {currentPageData.map((data, index) => (
-            <div className="col-lg-3 col-md-6 col-12" key={index}>
-              <div className="card-grow h-100 card">
-                <span className="badge-pricing">
-                  <div>{data.price === 0 ? 'free' : `Rp. ${data.price}`}</div>
-                </span>
-                <img
-                  src={`${process.env.NEXT_PUBLIC_API}/${data.image.nama}`}
-                  alt="semina"
-                />
-                <div className="card-content">
-                  <div className="card-title">{data.judul}</div>
-                  <div className="card-subtitle">{data.genre.nama}</div>
-                  <div className="description">
-                    {data.jenis}, {formatDate(data.rilis)}
+        {isLoading ? (
+          <div className="loader">Loading...</div>
+        ) : (
+          <div className="mt-5 row gap">
+            {currentPageData.map((data, index) => (
+              <div className="col-lg-3 col-md-6 col-12" key={index}>
+                <div className="card-grow h-100 card">
+                  <span className="badge-pricing">
+                    <div>{data.price === 0 ? 'free' : `Rp. ${data.price}`}</div>
+                  </span>
+                  <img
+                    src={`${process.env.NEXT_PUBLIC_API}/${data.image.nama}`}
+                    alt="semina"
+                  />
+                  <div className="card-content">
+                    <div className="card-title">{data.judul}</div>
+                    <div className="card-subtitle">{data.genre.nama}</div>
+                    <div className="description">
+                      {data.jenis}, {formatDate(data.rilis)}
+                    </div>
+                    <Link href={`/detail/${data._id}`}>
+                      <a className="stretched-link"></a>
+                    </Link>
                   </div>
-                  <Link href={`/detail/${data._id}`}>
-                    <a className="stretched-link"></a>
-                  </Link>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
 
       <div className="d-flex justify-content-center mt-4">
