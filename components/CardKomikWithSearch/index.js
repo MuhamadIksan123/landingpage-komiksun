5fc2124f951d3f76a6e89c0225952a5a70bd5edd
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-09 13:31:32 +0700

    update: forEach

--- components/CardKomikWithSearch/index.js
+++ components/CardKomikWithSearch/index.js
@@ -1,113 +1,111 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import CardKomikNoTitle from '../CardKomikNoTitle';
 import SearchInput from '../SearchInput';
 import SelectBox from '../SelectBox';
 /* eslint-disable @next/next/no-img-element */
 
-export default function CardKomikWithSearch({dataKomik, dataGenre}) {
-    const [filteredKomikData, setFilteredKomikData] = useState(dataKomik);
-    const [nameFilter, setNameFilter] = useState('');
-    const [genreFilter, setGenreFilter] = useState(null);
-    const [jenisFilter, setJenisFilter] = useState(null);
+export default function CardKomikWithSearch({ dataKomik, dataGenre }) {
+  const [filteredKomikData, setFilteredKomikData] = useState(dataKomik);
+  const [nameFilter, setNameFilter] = useState('');
+  const [genreFilter, setGenreFilter] = useState(null);
+  const [jenisFilter, setJenisFilter] = useState(null);
+  const [_temp, setTemp] = useState([]);
 
-    useEffect(() => {
-      if (!nameFilter || !genreFilter || !jenisFilter)
-        setFilteredKomikData(dataKomik);
-      if (nameFilter)
-        setFilteredKomikData(
-          dataKomik.filter((item) =>
-            item.judul.toLowerCase().includes(nameFilter.toLowerCase())
-          )
-        );
-      if (genreFilter)
-        setFilteredKomikData(
-          dataKomik.filter((item) => item.genre._id === genreFilter.value)
-        );
-      if (jenisFilter)
-        setFilteredKomikData(
-          dataKomik.filter(
-            (item) =>
-              item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
-          )
-        );
+  useEffect(() => {
+    if (!nameFilter || !genreFilter || !jenisFilter)
+      setFilteredKomikData(dataKomik);
+    if (nameFilter)
+      setFilteredKomikData(
+        dataKomik.filter((item) =>
+          item.judul.toLowerCase().includes(nameFilter.toLowerCase())
+        )
+      );
+    if (genreFilter)
+      setFilteredKomikData(
+        dataKomik.filter((item) => item.genre._id === genreFilter.value)
+      );
+    if (jenisFilter)
+      setFilteredKomikData(
+        dataKomik.filter(
+          (item) => item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
+        )
+      );
 
-      if (nameFilter && genreFilter)
-        setFilteredKomikData(
-          dataKomik.filter(
-            (item) =>
-              item.judul.toLowerCase().includes(nameFilter.toLowerCase()) &&
-              item.genre._id === genreFilter.value
-          )
-        );
+    if (nameFilter && genreFilter)
+      setFilteredKomikData(
+        dataKomik.filter(
+          (item) =>
+            item.judul.toLowerCase().includes(nameFilter.toLowerCase()) &&
+            item.genre._id === genreFilter.value
+        )
+      );
 
-      if (nameFilter && jenisFilter)
-        setFilteredKomikData(
-          dataKomik.filter(
-            (item) =>
-              item.judul.toLowerCase().includes(nameFilter.toLowerCase()) &&
-              item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
-          )
-        );
+    if (nameFilter && jenisFilter)
+      setFilteredKomikData(
+        dataKomik.filter(
+          (item) =>
+            item.judul.toLowerCase().includes(nameFilter.toLowerCase()) &&
+            item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
+        )
+      );
 
-      if (genreFilter && jenisFilter)
-        setFilteredKomikData(
-          dataKomik.filter(
-            (item) =>
-              item.genre._id === genreFilter.value &&
-              item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
-          )
-        );
+    if (genreFilter && jenisFilter)
+      setFilteredKomikData(
+        dataKomik.filter(
+          (item) =>
+            item.genre._id === genreFilter.value &&
+            item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
+        )
+      );
 
-      if (nameFilter && genreFilter && jenisFilter)
-        setFilteredKomikData(
-          dataKomik.filter(
-            (item) =>
-              item.judul.toLowerCase().includes(nameFilter.toLowerCase()) &&
-              item.genre._id === genreFilter.value &&
-              item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
-          )
-        );
-    }, [nameFilter, genreFilter, jenisFilter]);
+    if (nameFilter && genreFilter && jenisFilter)
+      setFilteredKomikData(
+        dataKomik.filter(
+          (item) =>
+            item.judul.toLowerCase().includes(nameFilter.toLowerCase()) &&
+            item.genre._id === genreFilter.value &&
+            item.jenis.toLowerCase() === jenisFilter.value.toLowerCase()
+        )
+      );
+  }, [nameFilter, genreFilter, jenisFilter]);
 
-    let jenisKomik = [
-      {
-        value: 'Manga',
-        label: 'Manga',
-        target: { value: 'Manga', name: 'jenis' },
-      },
-      {
-        value: 'Manhwa',
-        label: 'Manhwa',
-        target: { value: 'Manhwa', name: 'jenis' },
-      },
-      {
-        value: 'Manhua',
-        label: 'Manhua',
-        target: { value: 'Manhua', name: 'jenis' },
-      },
-      {
-        value: 'Webtoon',
-        label: 'Webtoon',
-        target: { value: 'Webtoon', name: 'jenis' },
-      },
-      {
-        value: 'Komik Indo',
-        label: 'Komik Indo',
-        target: { value: 'Komik Indo', name: 'jenis' },
-      },
-    ];
+  let jenisKomik = [
+    {
+      value: 'Manga',
+      label: 'Manga',
+      target: { value: 'Manga', name: 'jenis' },
+    },
+    {
+      value: 'Manhwa',
+      label: 'Manhwa',
+      target: { value: 'Manhwa', name: 'jenis' },
+    },
+    {
+      value: 'Manhua',
+      label: 'Manhua',
+      target: { value: 'Manhua', name: 'jenis' },
+    },
+    {
+      value: 'Webtoon',
+      label: 'Webtoon',
+      target: { value: 'Webtoon', name: 'jenis' },
+    },
+    {
+      value: 'Komik Indo',
+      label: 'Komik Indo',
+      target: { value: 'Komik Indo', name: 'jenis' },
+    },
+  ];
 
-    let res = dataGenre;
+  useEffect(() => {
+    const transformedData = dataGenre.map((item) => ({
+      value: item._id,
+      label: item.nama,
+      target: { value: item._id, name: 'genre' },
+    }));
 
-    let _temp = [];
-
-    res.forEach((res) => {
-      _temp.push({
-        value: res._id,
-        label: res.nama,
-        target: { value: res._id, name: 'genre' },
-      });
-    });
+    setTemp(transformedData);
+  }, [dataGenre]);
   return (
     <>
       <div className="container mt-5">
